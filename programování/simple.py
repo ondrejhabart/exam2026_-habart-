5ffb999196c022507f44c9ae8433081aaf046a75
habar <26shabart@dgkralupy.eu>
2026-04-13 08:20:20 +0200

    pridany slozky

---
+++ programování/simple.py
@@ -0,0 +1,11 @@
+objednavky = [
+    {"cislo": 1001, "zakaznik": "Jan Novák", "zaplaceno": True, "polozka": "Myš", "mnozstvi": 2, "cena_kus": 350},
+    {"cislo": 1001, "zakaznik": "Jan Novák", "zaplaceno": True, "polozka": "Klávesnice", "mnozstvi": 1, "cena_kus": 1200},
+    {"cislo": 1002, "zakaznik": "Petra Svobodová", "zaplaceno": False, "polozka": "Monitor", "mnozstvi": 1, "cena_kus": 4200},
+    {"cislo": 1003, "zakaznik": "Karel Dvořák", "zaplaceno": True, "polozka": "USB disk", "mnozstvi": 3, "cena_kus": 250},
+    {"cislo": 1004, "zakaznik": "Anna Malá", "zaplaceno": True, "polozka": "Notebook", "mnozstvi": 1, "cena_kus": 15000},
+    {"cislo": 1004, "zakaznik": "Anna Malá", "zaplaceno": True, "polozka": "Myš", "mnozstvi": 1, "cena_kus": 300}
+]
+
+vybrani = []
+
